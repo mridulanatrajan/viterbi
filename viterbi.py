dbb9e37b00ace538af1772534532a0342bb2ffbd
mridulanatrajan <mridulanatrajan@gmail.com>
2016-11-24 09:44:10 -0700

    third commit

--- viterbi.py
+++ viterbi.py
@@ -36,35 +36,40 @@ for t in set(tags):
     emision[t]={w[0]:w[2]/tc[t] for w in wordtags if w[1]==t and w[0]!='null' }
     transition[t]={tn:bigrams.count(t+tn)/tc[t] for tn in set(tags)}        
 
-def viterbi(s=[]):
-    trelis=np.zeros((len(tc),2+len(s)))     
-    pointer=np.zeros((len(tc),2+len(s)))   #back pointer trelis
-    trelis[1,0]=1                          #probability of start state in the trelis
-    im=np.zeros(2+len(tl))                 #intermediate array to store the calculated prababilities of transitioning from one state to another in each word
+tclen=len(tc)
+tllen=len(tl)
+
+def viterbi(sentence):
+    
+    slen=len(sentence)
+    trelis=np.zeros((tclen,2+slen))     
+    pointer=np.zeros((tclen,2+slen))   #back pointer trelis
+    trelis[1,0]=1                      #probability of start state in the trelis
+    im=np.zeros(2+tllen)               #intermediate array to store the calculated prababilities of transitioning from one state to another in each word
     
     #transitioning from <s> to the best probable state for the first word
-    for j in range (1,len(tl)):           
-        trelis[j,1]=transition['<s>'][tl[j]]*emision[tl[j]].get(s[0],0)
+    for j in range (1,tllen):           
+        trelis[j,1]=transition['<s>'][tl[j]]*emision[tl[j]].get(sentence[0],0)
         pointer[j,1]=0          
     
     #transitions in between the start state and end state        
-    for i in range (2,len(s)+1):  
-        for j in range (1,len(tl)):   
-            for k in range(1,len(tl)):
+    for i in range (2,slen+1):  
+        for j in range (1,tllen):   
+            for k in range(1,tllen):
                 im[k]=trelis[k,i-1]*transition[tl[k]][tl[j]]
-            trelis[j,i]=max(im)*emision[tl[j]].get(s[i-1],0) 
+            trelis[j,i]=max(im)*emision[tl[j]].get(sentence[i-1],0) 
             if trelis[j,i]>0:
                 pointer[j,i]=np.argmax(im)
     
     #transitioning from the best probable state for the last word to </s>             
-    for k in range(1,len(tl)):  
-        im[k]=trelis[k,len(s)]*transition[tl[k]]['</s>']    
-    trelis[46,len(s)+1]=max(im)
-    pointer[46,len(s)+1]=np.argmax(im)
+    for k in range(1,tllen):  
+        im[k]=trelis[k,slen]*transition[tl[k]]['</s>']    
+    trelis[46,slen+1]=max(im)
+    pointer[46,slen+1]=np.argmax(im)
     
     #tracing back the backpointer trelis from </s> to <s> moving backwards
-    trace=[tl[int(pointer[46,len(s)+1])]]  
-    for t in range(len(s),1,-1):         
+    trace=[tl[int(pointer[46,slen+1])]]  
+    for t in range(slen,1,-1):         
         trace.append(tl[int(pointer[tl.index(trace[-1]),t])])
     trace.reverse()
     
