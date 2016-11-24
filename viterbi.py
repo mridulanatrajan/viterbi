b2824df26c4dbf1bf121b053a8b2685c370619af
mridulanatrajan <mridulanatrajan@gmail.com>
2016-11-24 09:32:12 -0700

    second commit

--- viterbi.py
+++ viterbi.py
@@ -5,7 +5,8 @@ emision={}                  #emision probabilities
 transition={}               #transition probabilities
 bigrams=[]                  #tag bigrams
 
-lines=['null\t<s>']  #adding line boundaries   
+#adding line boundaries
+lines=['null\t<s>']     
 for line in open('wsj00-18.tag'):
     if "\t" in line:
         lines.append(line.strip())
@@ -13,47 +14,56 @@ for line in open('wsj00-18.tag'):
         lines.extend(['null\t</s>','null\t<s>'])
 del lines[-1]
 
-tags  = [l.split("\t")[1] for l in lines]  #list of tags in the same sequence as that of wsj corpus               
-tl=[i for i in set(tags) if i not in ('</s>','<s>')]  #list of the set tags without word boundaries
+#list of tags in the same sequence as that of wsj corpus
+tags  = [l.split("\t")[1] for l in lines]     
+
+#list of the set tags without word boundaries           
+tl=[i for i in set(tags) if i not in ('</s>','<s>')]  
 tl.insert(0, '<s>')
 
-tc=c.Counter(tags)  #tag counts
-lc= c.Counter(lines)  # word tag combination counts(line counts)
+tc=c.Counter(tags)     #tag counts
+lc= c.Counter(lines)   #word tag combination counts(line counts)
 wordtags = [(l[0].split("\t")[0],l[0].split("\t")[1],l[1]) for l in lc.items()]  
 
-for i, t in enumerate(tags): #storing bigram of tags in a list
+#storing bigram of tags in a list
+for i, t in enumerate(tags): 
     if i+1<len(tags):
         next=tags[i+1]
         bigrams.append(t+next)
 
-for t in set(tags):  # storing emision and transition probabilities
+# storing emision and transition probabilities
+for t in set(tags):  
     emision[t]={w[0]:w[2]/tc[t] for w in wordtags if w[1]==t and w[0]!='null' }
     transition[t]={tn:bigrams.count(t+tn)/tc[t] for tn in set(tags)}        
 
 def viterbi(s=[]):
     trelis=np.zeros((len(tc),2+len(s)))     
-    pointer=np.zeros((len(tc),2+len(s)))  #back pointer trelis
-    trelis[1,0]=1   #probability of start state in the trelis
-    im=np.zeros(2+len(tl)) #intermediate array to store the calculated prababilities of transitioning from one state to another in each word
+    pointer=np.zeros((len(tc),2+len(s)))   #back pointer trelis
+    trelis[1,0]=1                          #probability of start state in the trelis
+    im=np.zeros(2+len(tl))                 #intermediate array to store the calculated prababilities of transitioning from one state to another in each word
     
-    for j in range (1,len(tl)):  #transitioning from <s> to the best probable state for the first word         
+    #transitioning from <s> to the best probable state for the first word
+    for j in range (1,len(tl)):           
         trelis[j,1]=transition['<s>'][tl[j]]*emision[tl[j]].get(s[0],0)
         pointer[j,1]=0          
-            
-    for i in range (2,len(s)+1):  #transitions in between the start state and end state
+    
+    #transitions in between the start state and end state        
+    for i in range (2,len(s)+1):  
         for j in range (1,len(tl)):   
             for k in range(1,len(tl)):
                 im[k]=trelis[k,i-1]*transition[tl[k]][tl[j]]
             trelis[j,i]=max(im)*emision[tl[j]].get(s[i-1],0) 
             if trelis[j,i]>0:
                 pointer[j,i]=np.argmax(im)
-                
-    for k in range(1,len(tl)):  #transitioning from the best probable state for the last word to </s>
+    
+    #transitioning from the best probable state for the last word to </s>             
+    for k in range(1,len(tl)):  
         im[k]=trelis[k,len(s)]*transition[tl[k]]['</s>']    
     trelis[46,len(s)+1]=max(im)
     pointer[46,len(s)+1]=np.argmax(im)
     
-    trace=[tl[int(pointer[46,len(s)+1])]]  #tracing back the backpointer trelis from </s> to <s> moving backwards
+    #tracing back the backpointer trelis from </s> to <s> moving backwards
+    trace=[tl[int(pointer[46,len(s)+1])]]  
     for t in range(len(s),1,-1):         
         trace.append(tl[int(pointer[tl.index(trace[-1]),t])])
     trace.reverse()
